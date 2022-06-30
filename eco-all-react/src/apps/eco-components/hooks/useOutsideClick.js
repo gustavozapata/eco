10555c2822b267cb69c180a5d32fff3c0f6a0e00
Gustavo Zapata <tavordie@hotmail.com>
2022-06-30 07:52:46 +0100

    add hooks folder and click outside hook

---
+++ eco-all-react/src/apps/eco-components/hooks/useOutsideClick.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+// This hook allows us to detect a click outside of a component
+const useOutsideClick = (ref, callback) => {
+  const handleClick = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+};
+
+export default useOutsideClick;
