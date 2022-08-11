c69b37f5a2630f6eb61fbe671574754e0e5eaba4
Gustavo Zapata <tavordie@hotmail.com>
2022-08-11 10:05:29 +0100

    add container layout component

---
+++ eco-all-react/src/apps/eco-layout/layout/Container/Container.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { addClassNames, addPropKeysAsClass } from "../../utils";
+import "./Container.css";
+
+/**
+DOCS:
+* This wraps elements to keep them within website boundaries (max-width: 1200, etc)
+* If an element goes outside these boundaries, the Container is close </Container> and this element is wrapped in a new <Section>
+*/
+
+const Container = (props) => {
+  const { children, className, ...rest } = props;
+
+  return (
+    <div
+      className={`Container ${addPropKeysAsClass(
+        "Container",
+        rest
+      )} ${addClassNames(className)}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
