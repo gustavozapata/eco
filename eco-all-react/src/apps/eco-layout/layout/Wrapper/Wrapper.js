9a429d77da33b44e5cf715c7461727bc3d8a9c41
Gustavo Zapata <tavordie@hotmail.com>
2022-08-17 08:51:08 +0100

    add wrapper layout component

---
+++ eco-all-react/src/apps/eco-layout/layout/Wrapper/Wrapper.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { addClassNames, addPropKeysAsClass } from "../../utils";
+import "./Wrapper.css";
+
+/**
+DOCS:
+* This is a full width (occupies full width of parent) wrapper that fits in most parent elements and can be flex, absolute, etc
+*/
+
+// TODO: an element can have 'style', 'classes', 'events', 'id'
+const generateClasses = (classNames) => {};
+const generateStyles = () => {};
+
+const Wrapper = (props) => {
+  const { children, className, ...rest } = props;
+  return (
+    <div
+      // className={generateClasses(className)}
+      // style={generateStyles()}
+      className={`Wrapper ${addClassNames(className)} ${addPropKeysAsClass(
+        "Wrapper",
+        rest
+      )}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Wrapper;
